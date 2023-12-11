be085b549f4204e8546bd3b6d5dab45dcff12456
GDS K S <gagandua078@gmail.com>
2023-12-10 20:34:23 -0600

    Minor bug fixes

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { savePaymentMethod } from '../actions/cartActions';
+import paypalIcon from '../images/paypal-icon.png';
+
 function PaymentScreen({ history }) {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
@@ -54,7 +56,7 @@ function PaymentScreen({ history }) {
                 label={
                   <div className="d-flex align-items-center">
                     <img
-                      src="/images/paypal-icon.png" // Replace with your PayPal or Credit Card icon
+                      src={paypalIcon} // Replace with your PayPal or Credit Card icon
                       alt="PayPal or Credit Card"
                       className="me-3"
                       style={{ maxWidth: '40px' }} // Adjust the icon size as needed
